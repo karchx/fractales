fff7a57a6292c69096328db99f1f964a11257b7e
Стив <kenethra.74@gmail.com>
2023-06-13 20:39:38 -0600

    feat: cli flags

---
+++ fractal.go
@@ -0,0 +1,16 @@
+package main
+
+type Location struct {
+	XCenter float64
+	YCenter float64
+	Zoom    float64
+}
+
+type LocationFile struct {
+	Locations []Location
+}
+
+type ImageConfig struct {
+	Width  int
+	Height int
+}
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"image"
+	"image/png"
+	"os"
+	"strconv"
+
+	log "github.com/gothew/l-og"
+)
+
+var imgConf ImageConfig
+
+func main() {
+	parseImageConfigArgs()
+}
+
+func parseImageConfigArgs() {
+	imgWidthPtr := flag.Int("width", 1920, "The width of the image in pixels.")
+	imgHeightPtr := flag.Int("height", 1024, "The height of the image in pixels.")
+
+	flag.Parse()
+
+	imgConf = ImageConfig{
+		Width:  *imgWidthPtr,
+		Height: *imgHeightPtr,
+	}
+}
+
+func generateImagesFromLocations(locs LocationFile) {
+	if _, err := os.Stat("results/" + strconv.Itoa(500)); os.IsNotExist(err) {
+		os.Mkdir("results/"+strconv.Itoa(500), 0755)
+	}
+
+	for index, loc := range locs.Locations {
+		log.Infof("Allocating and rendering image %d\n", index+1)
+		img := image.NewRGBA(image.Rect(0, 0, imgConf.Width, imgConf.Height))
+
+		log.Info(loc)
+		log.Infof("Encoding image %d\n", index+1)
+		filename := "results/" + strconv.Itoa(500) + "/" + strconv.Itoa(index+1)
+		f, err := os.Create(filename + ".png")
+		if err != nil {
+			panic(err)
+		}
+
+		err = png.Encode(f, img)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
