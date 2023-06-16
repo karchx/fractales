e751c4e7e8444990d8e604528ffd9504815a7cc8
Стив <kenethra.74@gmail.com>
2023-06-15 21:45:56 -0600

    feat: rand number

---
+++ rand.go
@@ -0,0 +1,16 @@
+package main
+
+// RandUint64 algorithm random int value.
+func RandUint64(rng *uint64) uint64 {
+  *rng = *rng*0x3243f6a8885a308d + 1
+	r := *rng
+	r ^= r >> 32
+	r *= 1111111111111111111
+	r ^= r >> 32
+	return r
+}
+
+// RandFloat64 random float value.
+func RandFloat64(rng *uint64) float64 {
+  return float64(RandUint64(rng)/2) / (1 << 63)
+}
